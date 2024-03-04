24376d6ba5b8ec2268129aa35dcba20b72d9356f
jaeho <ejaho0103@yonsei.ac.kr>
2024-03-03 21:33:46 -0500

    commit

--- configs/conform.lua
+++ configs/conform.lua
@@ -1,11 +1,12 @@
 local conform = require "conform"
 
-conform.setup({
+conform.setup {
   formatters_by_ft = {
     lua = { "stylua" },
     -- Use a sub-list to run only the first available formatter
     javascript = { { "prettierd", "prettier" } },
-    cpp = { "clang_format"},
+    cpp = { "clang_format" },
+    python = { "black" },
   },
   format_on_save = {
     timeout_ms = 1000,
@@ -14,11 +15,10 @@ conform.setup({
   format_after_save = {
     lsp_fallback = true,
   },
-})
-
+}
 
 conform.formatters.clang_format = {
   prepend_args = {
-    "-style={BasedOnStyle: WebKit, AlignTrailingComments: true, IndentWidth: 2, TabWidth: 2, UseTab: Always}"
+    "-style={BasedOnStyle: WebKit, AlignTrailingComments: true, IndentWidth: 2, TabWidth: 2, UseTab: Never}",
   },
 }
